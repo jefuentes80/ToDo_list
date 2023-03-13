9dda7fa87615c845c0d4454df6d44110e74cc058
Jossue <jefuentes@scl.edu.gt>
2023-03-13 12:48:34 -0600

    Todo Actualizado

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
+// Se importa la función imprimir_todos
 import { imprimir_todos } from "./cargar-los-todos.js";
 
 imprimir_todos();
 
+
 function nueva_tarea (){
+
+    // Recuperarndo el valor de la caja de texto input
+    let mensaje = document.querySelector(".texto").value;
+
+    /* 
+    * Con el método push agregamos un nuevo elemento a la
+    * lista, ya que la lista contiene objetos como elementos
+    * los datos ha agregar deben tener el mismo patron
+    */
+
     data_todo_list.push(
         {
-            id : "4",
-            mensaje : "Arreglar la cama",
+            id : "5",
+            mensaje : mensaje,
             estado: true
         }
     );
 
+    // limpiamos la lista de todo list anteriores
     document.querySelector(".todo_list").innerHTML = "";
+    // Imprime la lista nuevamente
     imprimir_todos();
-
+    // Oculta la ventana modal
     document.querySelector(".modal").classList.remove("activar");
+    // Limpia el contenido de la caja de texto
+    document.querySelector(".texto").value = " ";
 }
 
-document.querySelector(".btn-tarea").addEventListener("click",nueva_tarea);
+
+document.querySelector(".btn-crear-tarea").addEventListener("click",nueva_tarea);
--- js/item-component.js
+++ js/item-component.js
@@ -3,9 +3,9 @@ export function  item_component(numero_tarea){
     let  item= `
     <div class="todo_item">
         <span class="ico">☑️</span>
-            <p>
-                ${data_todo_list[numero_tarea].mensaje}
-            </p>
+        <p>
+            ${data_todo_list[numero_tarea].mensaje}
+        </p>
         <span class="ico2">X</span>
     </div>`;
 
